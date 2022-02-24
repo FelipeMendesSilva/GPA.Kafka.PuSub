c3233af6d1059e9a11d3bd5a37a81038a7546c0a
febasmendes <febasmendes@gmail.com>
2022-02-24 15:33:43 -0300

    commit

---
+++ GPA.Kafka.PuSub/Models/ConfigurationModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GPA.Kafka.PuSub.Models
+{
+    public class ConfigurationModel
+    {
+        public string BootstrapServers { get; set; }
+        public string Topic { get; set; }
+        public string ClientId { get; set; }
+        public bool EnableAutoCommit { get; set; }
+        public bool EnableAutoOffsetStore { get; set; }
+        public int AutoOffsetReset { get; set; }
+    }
+}
--- GPA.Kafka.PuSub/Pages/Index.cshtml.cs
+++ GPA.Kafka.PuSub/Pages/Index.cshtml.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Confluent.Kafka;
+using GPA.Kafka.PuSub.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GPA.Kafka.PuSub.Pages
@@ -17,8 +20,47 @@ namespace GPA.Kafka.PuSub.Pages
             _logger = logger;
         }
 
+        [BindProperty]
+        public ConfigurationModel Config { get; set; }
+        [BindProperty]
+        public string Message { get; set; }
+
         public void OnGet()
         {
+
+        }
+
+        public void OnPost()
+        {
+
+        }
+
+        public void OnPostPublishAsync(CancellationToken stoppingToken)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = Config.BootstrapServers,
+                ClientId = Config.ClientId
+            };
+
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                var t = producer.ProduceAsync("topic", new Message<Null, string> { Value = Message }, stoppingToken);
+                t.ContinueWith(task =>
+                {
+                    if (task.IsFaulted)
+                    {
+                        //msg nao enviada
+                    }
+                    else
+                    {
+                        //mensagem enviada
+                    }
+                });
+                producer.Flush(TimeSpan.FromSeconds(5));
+                producer.Dispose();
+            }
 
         }
     }
